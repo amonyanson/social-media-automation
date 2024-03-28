6a62792c7bd878aa31cf572a8ef655d1696e6bea
AFT3RM@TH <amonianson@gmail.com>
2024-03-28 16:25:28 +0300

    initial commit

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -1,4 +1,12 @@
-bot = Bot()
-bot.login(username = "notsxkhan",password="commando")
+from instagrapi import Client
 
-bot.upload_photo("insertpicturehere.jpg",caption="Your caption")
+with open("credentials.txt","r") as f:
+    username, password = f.read().splitlines()
+client = Client()
+client.login(username,password)
+
+hashtag = "programming"
+media = client.hashtag_medias_recent(hashtag,20)
+
+for i, media in enumerate(media):
+    client.media_like(media.id)
